ffec91788c9925b218241824e76077c69bd24c19
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-06-25 22:35:44 -0500

    agregar personajes y listarlos en componentes

---
+++ src/app/dbz/components/list-characters/list-characters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-list-characters',
+  templateUrl: './list-characters.component.html',
+  styleUrls: ['./list-characters.component.css']
+})
+export class ListCharactersComponent {
+
+}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ListCharactersComponent } from './components/list-characters/list-characters.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
@@ -9,7 +11,9 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
     MainPageComponent,
   ],
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListCharactersComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule
