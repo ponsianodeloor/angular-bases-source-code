b64d8e1d709f30fcc7d1cd572c8693a2ccc314e2
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-06-25 21:12:32 -0500

    cargar pagina principal del componente dbz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from "./dbz/dbz.module";
 
 import { AppComponent } from './app.component';
+
 
 
 @NgModule({
@@ -15,6 +17,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
